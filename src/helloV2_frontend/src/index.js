79a86a333da40918b0deb99884470007b35de89c
root <root@Shrinivas>
2023-09-27 11:28:46 +0530

    Initial commit.

---
+++ src/helloV2_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { helloV2_backend } from "../../declarations/helloV2_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await helloV2_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
